d499f8304139c8d71b64034d72fc00339cc3d577
Lookey <lookeypl@gmail.com>
2024-08-26 20:41:16 +0200

    Change cwd only when inside the repository
    This is done to access correct directories and not leave the confines of
    exe's directory when publishing the binary

--- LukeBot.Common/FileUtils.cs
+++ LukeBot.Common/FileUtils.cs
@@ -8,7 +8,12 @@ namespace LukeBot.Common
         {
             string cwd = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             string newCwd = cwd + "/../../..";
-            Directory.SetCurrentDirectory(newCwd);
+
+            // DEV
+            // Changes current directory to repo root to access Data directory
+            // Otherwise, after publishing data this won't be accessible so CWD will be the exe location
+            if (File.Exists(newCwd + "/LukeBot.sln"))
+                Directory.SetCurrentDirectory(newCwd);
         }
 
         public static bool Exists(string path)
